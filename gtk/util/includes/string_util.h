44c6046f5f3ec7a78f3f09579f0ebe2fb5fc2977
GZYangKui <752544765@qq.com>
2021-07-06 18:10:40 +0800

    fix bug.

---
+++ gtk/util/includes/string_util.h
@@ -0,0 +1,8 @@
+//
+// Created by yangkui on 2021/7/6.
+//
+
+#ifndef X_LITE_STRING_UTIL_H
+#define X_LITE_STRING_UTIL_H
+
+#endif //X_LITE_STRING_UTIL_H
---
+++ gtk/util/string_util.c
@@ -0,0 +1,5 @@
+//
+// Created by yangkui on 2021/7/6.
+//
+
+#include "string_util.h"
